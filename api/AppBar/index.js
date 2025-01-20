aa62291ee3e5913970ecdafe26fd5ebf8acd5fa9
kyutekrap <katepark1225@naver.com>
2025-01-20 22:41:17 +0900

    Edit AppBar

--- api/AppBar/index.js
+++ api/AppBar/index.js
@@ -2,34 +2,26 @@ class AppBarExt {
 
     _appBar = null;
 
-    constructor(L, C, R) {
+    constructor(L, R) {
         this._appBar = document.createElement("div");
         this._appBar.classList.add("appBar");
         const lDiv = document.createElement("div");
         lDiv.classList.add("hBox");
-        const cDiv = document.createElement("div");
-        cDiv.classList.add("hBox");
-        cDiv.style.flex = 1;
-        cDiv.style.alignItems = "center";
         const rDiv = document.createElement("div");
         rDiv.classList.add("hBox");
         for (var i=0; i < L.length; i++) {
             lDiv.appendChild(L[i]);
         }
-        for (var i=0; i < C.length; i++) {
-            cDiv.appendChild(C[i]);
-        }
         for (var i=0; i < R.length; i++) {
             rDiv.appendChild(R[i]);
         }
         this._appBar.appendChild(lDiv);
-        this._appBar.appendChild(cDiv);
         this._appBar.appendChild(rDiv);
         document.body.appendChild(this._appBar);
     }
 }
 
-export function AppBar({L = [], C = [], R = []} = {}) {
-    const appBarExt = new AppBarExt(L, C, R);
+export function AppBar({L = [], R = []} = {}) {
+    const appBarExt = new AppBarExt(L, R);
     return appBarExt._appBar;
 }
