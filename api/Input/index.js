d94ad50c0ac02eeb9fe0b7a251dc220cc3a6b689
kyutekrap <katepark1225@naver.com>
2025-02-08 15:07:55 +0900

    Edit Input

--- api/Input/index.js
+++ api/Input/index.js
@@ -2,17 +2,18 @@ class InputExt {
 
     _input = null;
 
-    constructor(variant, editable, placeholder, content) {
+    constructor(variant, editable, placeholder, content, width) {
         this._input = document.createElement("input");
         this._input.classList.add("input");
         this._input.value = content;
         this._input.placeholder = placeholder;
         this._input.readOnly = !editable;
         this._input.type = variant;
+        this._input.style.width = width;
     }
 }
 
-export function Input({variant="text", editable=true, placeholder="", content=""} = {}) {
-    const inputExt = new InputExt(variant, editable, placeholder, content);
+export function Input({variant="text", editable=true, placeholder="", content="", width="100%"} = {}) {
+    const inputExt = new InputExt(variant, editable, placeholder, content, width);
     return inputExt._input;
 }
