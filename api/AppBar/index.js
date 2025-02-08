7a477bd84c5d0f95bfcf5b4da9126d2a48b552e2
kyutekrap <katepark1225@naver.com>
2025-02-08 15:24:59 +0900

    Edit AppBar

--- api/AppBar/index.js
+++ api/AppBar/index.js
@@ -1,27 +1,17 @@
+import { HBox } from "../HBox/index.js";
+
 class AppBarExt {
 
     _appBar = null;
 
-    constructor(L, R) {
-        this._appBar = document.createElement("div");
+    constructor(args) {
+        this._appBar = HBox(...args);
         this._appBar.classList.add("appBar");
-        const lDiv = document.createElement("div");
-        lDiv.classList.add("hBox");
-        const rDiv = document.createElement("div");
-        rDiv.classList.add("hBox");
-        for (var i=0; i < L.length; i++) {
-            lDiv.appendChild(L[i]);
-        }
-        for (var i=0; i < R.length; i++) {
-            rDiv.appendChild(R[i]);
-        }
-        this._appBar.appendChild(lDiv);
-        this._appBar.appendChild(rDiv);
         document.body.appendChild(this._appBar);
     }
 }
 
-export function AppBar({L = [], R = []} = {}) {
-    const appBarExt = new AppBarExt(L, R);
+export function AppBar(...args) {
+    const appBarExt = new AppBarExt(args);
     return appBarExt._appBar;
 }
