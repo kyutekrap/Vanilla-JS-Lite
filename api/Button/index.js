9f7187a510176d4e3694b62add2e4e9efc01889b
kyutekrap <katepark1225@naver.com>
2025-02-08 15:09:40 +0900

    Edit Button

--- api/Button/index.js
+++ api/Button/index.js
@@ -2,13 +2,14 @@ class ButtonExt {
 
     _button = null;
 
-    constructor(variant, size, content, onClick, type) {
+    constructor(variant, size, content, onClick, type, width) {
         this._button = document.createElement("button");
         this._button.classList.add("button");
         this._button.classList.add(variant);
         this._button.classList.add(size);
         this._button.innerHTML = content;
         this._button.type = type;
+        this._button.style.width = width;
         if (onClick.constructor.name === "AsyncFunction") {
             function onClickWithCallBack() {
                 this._button.classList.add("active");
@@ -27,7 +28,7 @@ class ButtonExt {
     }
 }
 
-export function Button({variant="filled", size="medium", content="", onClick=()=>{}, type="button"} = {}) {
-    const buttonExt = new ButtonExt(variant, size, content, onClick, type);
+export function Button({variant="filled", size="medium", content="", onClick=()=>{}, type="button", width="100%"} = {}) {
+    const buttonExt = new ButtonExt(variant, size, content, onClick, type, width);
     return buttonExt._button;
 }
