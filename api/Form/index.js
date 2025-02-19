165d9dc6aa22f5fc7ddafa735768c7ad280edb80
kyutekrap <katepark1225@naver.com>
2025-02-19 22:34:24 +0900

    Form Edit

--- api/Form/index.js
+++ api/Form/index.js
@@ -2,17 +2,20 @@ class FormExt {
 
     _form = null;
 
-    constructor(layout, fieldsets) {
+    constructor(layout, fieldsets, onSubmit) {
         this._form = document.createElement("form");
         this._form.classList.add("form");
         this._form.classList.add(layout === "vertical" ? "vbox" : "hbox");
         for (var i=0; i < fieldsets.length; i++) {
             this._form.appendChild(fieldsets[i]);
         }
+        this._form.addEventListener("submit", (e) => {
+            onSubmit(e);
+        });
     }
 }
 
-export function Form({layout="vertical", fieldsets=[]} = {}) {
-    const formExt = new FormExt(layout, fieldsets);
+export function Form({layout="vertical", onSubmit=(e)=>{}, fieldsets=[]} = {}) {
+    const formExt = new FormExt(layout, fieldsets, onSubmit);
     return formExt._form;
 }
